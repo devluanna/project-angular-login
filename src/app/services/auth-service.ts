a2399ddca5b812724b8e4a4106d01f5059a068cb
Luana Alves Nogueira <luanagcontato@gmail.com>
2024-06-17 07:31:43 -0300

    update services

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private user = {
+    first_name: '',
+    last_name: ''
+  };
+
+  
+  setUser(firstName: string, lastName: string): void {
+    this.user.first_name = firstName;
+    this.user.last_name = lastName;
+    console.log(firstName)
+  }
+
+  
+  getFirstName(): string {
+    return this.user.first_name;
+  }
+
+
+  getLastName(): string {
+    return this.user.last_name;
+  }
 
   constructor() {}
 
@@ -11,4 +32,10 @@ export class AuthService {
     const authToken = sessionStorage.getItem('auth-token');
     return !!authToken;
   }
+
+  getUsername(): string | null {
+    const username = sessionStorage.getItem('username');
+    return username !== null ? username : null;
+  }
+
 }
--- src/app/services/login-service.ts
+++ src/app/services/login-service.ts
@@ -1,22 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoginResponse } from '../../app/types/login-response.type';
 import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-    apiUrl: string = "http://localhost:8080/auth"
+  apiUrl: string = "http://localhost:8080/auth";
 
   constructor(private httpClient: HttpClient) { }
 
   login(identity: string, password: string){
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { identity, password }).pipe(
-      tap((value) => {
-        sessionStorage.setItem("auth-token", value.token)
-        sessionStorage.setItem("username", value.name)
+    return this.httpClient.post<any>(`${this.apiUrl}/login`, { identity, password }).pipe(
+      tap((response) => {
+        sessionStorage.setItem("auth-token", response.token);
+        sessionStorage.setItem("userId", response.id_user.toString());
+        console.log("token", response.token);
+        console.log("id_user", response.id_user);
       })
-    )
+    );
   }
 }
