a50eb46df8e2a4792c341bec0abba96e3f20995f
Luana Alves Nogueira <luanagcontato@gmail.com>
2024-06-29 23:48:35 -0300

    update: creation of the modal

--- src/app/components/success-modal/success-modal.component.ts
+++ src/app/components/success-modal/success-modal.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-success-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './success-modal.component.html',
   styleUrl: './success-modal.component.scss'
 })
 export class SuccessModalComponent {
+  @Input() successMessage: string = '';
+  @Input() isOpen: boolean = false;
+
+  closeModal() {
+    this.isOpen = false;
+  }
 
 }
