8179e0feeba74a19452eb29343cc2a797554077b
devluanna <luanagcontato@gmail.com>
2024-09-20 21:39:54 -0300

    reverter repository

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,7 +58,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-
+import 'core-js/es';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
